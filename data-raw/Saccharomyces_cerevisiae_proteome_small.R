3e2cf05b489732d547ccb9aa1945a59be53bb972
jmmoses99 <jmmoses99@gmail.com>
2025-11-30 01:27:18 -0500

    Update Saccharomyces_cerevisiae_proteome_small.R

--- data-raw/Saccharomyces_cerevisiae_proteome_small.R
+++ data-raw/Saccharomyces_cerevisiae_proteome_small.R
@@ -1,10 +1,10 @@
-#' Small Saccharomyces cerevisiae proteome subset for testing
+#' Small yeast proteome subset for testing
 #'
-#' A subset of the Saccharomyces cerevisiae proteome containing
-#' the first 3 proteins. This is intended for **unit testing**
-#' and fast examples without loading the full proteome.
-#'
-#' @format An \code{\link[Biostrings]{AAStringSet}} object of length 3.
-#' @source Generated from the full Saccharomyces cerevisiae proteome (UP000002311).
+#' A small example proteome subset of *Saccharomyces cerevisiae* used for internal
+#' testing and demonstration of IDPR functions.
+#' @format An `AAStringSet` (or other appropriate class) with N sequences.
+#'   The names (accessions) and sequences can be used to test cleaning and IDPR processing.
+#' @source Internal to the package for testing purposes.
 "Saccharomyces_cerevisiae_proteome_small"
+
 
